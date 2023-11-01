7e810915c128d526ba3a88ffbedfdf8af0da0696
Kyle-912 <kdt912@outlook.com>
2023-11-01 14:36:41 +0000

    Nov 1, 2023, 10:36 AM

--- Example/Core/Src/main.c
+++ Example/Core/Src/main.c
@@ -101,7 +101,7 @@ int main(void)
   while (1)
   {
     HAL_Delay(100);
-    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);//test
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
     if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) == GPIO_PIN_RESET)
     {
