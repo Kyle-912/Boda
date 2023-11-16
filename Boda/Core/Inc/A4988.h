412a2ca1733da54ae38b9e38d3446c9d9c1b84fc
Kyle-912 <kdt912@outlook.com>
2023-11-16 15:13:46 -0500

    updated motor drivers

--- Boda/Core/Inc/A4988.h
+++ Boda/Core/Inc/A4988.h
@@ -1,26 +1,49 @@
 #ifndef A4988_H
 #define A4988_H
 
-// Define the A4988 struct
-struct A4988 {
+#include "stm32f4xx_hal.h"
+
+typedef struct {
     short steps;
-    short dir_pin;
-    short step_pin;
-    short ms1_pin;
-    short ms2_pin;
-    short ms3_pin;
-    short enable_pin;
-    long steps_to_cruise;
-    long steps_remaining;
     short dir_state;
-    long steps_to_brake;
-    long step_pulse;
-    long cruise_step_pulse;
-    long rest;
+    short enable_pin;
     long step_count;
-};
+	long steps_remaining;
+    short microsteps;
+    float rpm;
+    long step_pulse;
+
+    GPIO_TypeDef * dir_port;
+    uint16_t dir_pin;
+
+    GPIO_TypeDef * step_port;
+    uint16_t step_pin;
+
+    GPIO_TypeDef * ms1_port;
+    uint16_t ms1_pin;
+
+    GPIO_TypeDef * ms2_port;
+    uint16_t ms2_pin;
+
+    GPIO_TypeDef * ms3_port;
+    uint16_t ms3_pin;
+
+    TIM_HandleTypeDef * timer;
+} stepper;
+
+	// long steps_to_brake;
+	// long step_pulse;
+    // long cruise_step_pulse;
+	// long rest;
+	// long step_count;
 
 // Function declaration for initializing the A4988 struct
-struct A4988 initA4988(short steps, short dir_pin, short step_pin, short ms1_pin, short ms2_pin, short ms3_pin);
+// struct stepper init(short steps, short dir_pin, short step_pin, short ms1_pin, short ms2_pin, short ms3_pin);
+
+void init_dir_pin(stepper *motor, GPIO_TypeDef *port, uint16_t pin);
+void init_step_pin(stepper *motor, GPIO_TypeDef *port, uint16_t pin);
+void init_ms1_pin(stepper *motor, GPIO_TypeDef *port, uint16_t pin);
+void init_ms2_pin(stepper *motor, GPIO_TypeDef *port, uint16_t pin);
+void init_ms3_pin(stepper *motor, GPIO_TypeDef *port, uint16_t pin);
 
 #endif // A4988_H
