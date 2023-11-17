6aaf873d3f5800908493c27021bdb64a08827196
Kyle-912 <kdt912@outlook.com>
2023-11-17 13:31:28 -0500

    attempted to integrate controller but commented it out

--- Boda/Core/Src/main.c
+++ Boda/Core/Src/main.c
@@ -150,7 +150,7 @@ int main(void)
 
   float rpm = 400;
   short microsteps = FULL_STEPS;
-  double deg = 720;
+  double deg = 60;
   const short spr = 200;
 
   stepper stepper_motor_1;
@@ -169,14 +169,14 @@ int main(void)
   while (1)
   {
 
-    // HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
-    if (toggle)
-    {
+    // if (toggle)
+    // {
       move_stepper_deg(motor1, deg);
-      HAL_Delay(500);
-      toggle = false;
-    }
+      HAL_Delay(5000);
+      // toggle = false;
+    // }
 
     PS2_TEST(&hspi2);
     // if the button is x
