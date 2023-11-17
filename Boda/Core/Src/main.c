f361d6aa6449560910c08aa08e9db7b88ad23fa4
Kyle-912 <kdt912@outlook.com>
2023-11-17 13:10:41 -0500

    fucked with it

--- Boda/Core/Src/main.c
+++ Boda/Core/Src/main.c
@@ -55,10 +55,10 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
-stepper* motor1 = NULL;
-stepper* motor2 = NULL;
-stepper* motor3 = NULL;
-stepper* motor4 = NULL;
+stepper *motor1 = NULL;
+stepper *motor2 = NULL;
+stepper *motor3 = NULL;
+stepper *motor4 = NULL;
 
 /* USER CODE END PV */
 
@@ -80,9 +80,9 @@ uint8_t *PS2DataIn[9];
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -117,7 +117,6 @@ int main(void)
   HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(TIM3_IRQn);
 
-
   char *messageX = "X has been Pressed\r\n";
   char *messageO = "O has been Pressed\r\n";
 
@@ -128,7 +127,6 @@ int main(void)
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);   // 4 - 5V   = PB_5
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET); // 3 - PWM  = PB_8
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);  // 2 - 5V   = PA_10
-
 
   //===================================================================================
   // int Dire  = 2;    //GPIO2 in Arduino UNO --- Direction of stepper motor driver
@@ -148,14 +146,11 @@ int main(void)
   bool toggle = true;
   // TIM3->CR1 |= TIM_CR1_CEN;
 
-  
-
   // HAL_TIM_Base_Start_IT(&htim3);
 
-
-  float rpm = 300;
+  float rpm = 400;
   short microsteps = FULL_STEPS;
-  double deg = 60;
+  double deg = 720;
   const short spr = 200;
 
   stepper stepper_motor_1;
@@ -171,31 +166,25 @@ int main(void)
   set_timer(motor1, &htim3);
   set_rpm(motor1, rpm);
 
-
   while (1)
   {
 
-    move_stepper_deg(motor1, deg);
-    HAL_Delay(5000);
-
     // HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 
-    // if (toggle)
-    // {
-    //   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
-    //   TIM3->CNT = 0;
-    //   while (TIM3->CNT < 40);
-    //   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
-    //   while (TIM3->CNT < 540);
-    // }
+    if (toggle)
+    {
+      move_stepper_deg(motor1, deg);
+      HAL_Delay(500);
+      toggle = false;
+    }
 
-    // PS2_TEST(&hspi2);
-    // // if the button is x
-    // if (PS2DataIn[5] == 0xbf)
-    // {
-    //   HAL_UART_Transmit(&huart2, (uint8_t *)messageX, strlen(messageX), 100);
-    //   toggle = true;
-    // }
+    PS2_TEST(&hspi2);
+    // if the button is x
+    if (PS2DataIn[5] == 0xbf)
+    {
+      HAL_UART_Transmit(&huart2, (uint8_t *)messageX, strlen(messageX), 100);
+      toggle = true;
+    }
     // else if (PS2DataIn[5] == 0xdf)
     // {
     //   HAL_UART_Transmit(&huart2, (uint8_t *)messageO, strlen(messageO), 100);
@@ -209,22 +198,22 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -241,9 +230,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -256,10 +244,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_SPI2_Init(void)
 {
 
@@ -290,14 +278,13 @@ static void MX_SPI2_Init(void)
   /* USER CODE BEGIN SPI2_Init 2 */
 
   /* USER CODE END SPI2_Init 2 */
-
 }
 
 /**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM1_Init(void)
 {
 
@@ -312,7 +299,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 72-1;
+  htim1.Init.Prescaler = 72 - 1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -336,14 +323,13 @@ static void MX_TIM1_Init(void)
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
-
 }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM3_Init(void)
 {
 
@@ -381,14 +367,13 @@ static void MX_TIM3_Init(void)
   /* USER CODE BEGIN TIM3_Init 2 */
 
   /* USER CODE END TIM3_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -414,19 +399,18 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -435,10 +419,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_8, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
@@ -450,14 +434,14 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LD2_Pin PA8 PA9 PA10 */
-  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Pin = LD2_Pin | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB12 PB4 PB5 PB8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_8;
+  GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -470,41 +454,46 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
 // Callback function
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-    if (htim->Instance == TIM3) {
-        motor1->steps_remaining--;
-        if (motor1->steps_remaining <= 0)
-        {
-          HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, RESET);
-          HAL_TIM_Base_Stop_IT(htim);
-        }
-        // We should pull HIGH for at least 1-2us (step_high_min)
-        else {
-          GPIO_PinState currentPinState = HAL_GPIO_ReadPin(motor1->step_port, motor1->step_pin);
-          if (currentPinState == GPIO_PIN_SET){
-            HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, RESET);
-            __HAL_TIM_SET_AUTORELOAD(motor1->timer, motor1->step_pulse);
-            // motor1->timer->Instance->ARR = motor1->step_pulse;
-          }
-          else {
-            HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, SET);
-            __HAL_TIM_SET_AUTORELOAD(motor1->timer, 20);
-            // motor1->timer->Instance->ARR = 40;
-          }
-          __HAL_TIM_SET_COUNTER(motor1->timer, 0);
-        }
-        // Code to execute when the timer interrupt occurs
-        // char *message1 = "Timer Elapsed\r\n";
-        // HAL_UART_Transmit(&huart2, (uint8_t *)message1, strlen(message1), 100);
-        // For example, toggle an LED or send a message
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim->Instance == motor1->timer->Instance)
+  {
+    motor1->steps_remaining--;
+    if (motor1->steps_remaining <= 0)
+    {
+      HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, RESET);
+      HAL_TIM_Base_Stop_IT(htim);
     }
+    // We should pull HIGH for at least 1-2us (step_high_min)
+    else
+    {
+      GPIO_PinState currentPinState = HAL_GPIO_ReadPin(motor1->step_port, motor1->step_pin);
+      if (currentPinState == GPIO_PIN_SET)
+      {
+        HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, RESET);
+        __HAL_TIM_SET_AUTORELOAD(motor1->timer, motor1->step_pulse);
+        // motor1->timer->Instance->ARR = motor1->step_pulse;
+      }
+      else
+      {
+        HAL_GPIO_WritePin(motor1->step_port, motor1->step_pin, SET);
+        __HAL_TIM_SET_AUTORELOAD(motor1->timer, 20);
+        // motor1->timer->Instance->ARR = 40;
+      }
+      __HAL_TIM_SET_COUNTER(motor1->timer, 0);
+    }
+    // Code to execute when the timer interrupt occurs
+    // char *message1 = "Timer Elapsed\r\n";
+    // HAL_UART_Transmit(&huart2, (uint8_t *)message1, strlen(message1), 100);
+    // For example, toggle an LED or send a message
+  }
 }
 
 static void PS2_TEST(SPI_HandleTypeDef *hspi)
@@ -555,16 +544,17 @@ void delay_2_25us(uint16_t us)
 {
   TIM1->CNT = 0;
   TIM1->CR1 |= TIM_CR1_CEN;
-  while (TIM1->CNT < us);
+  while (TIM1->CNT < us)
+    ;
   TIM1->CR1 |= TIM_CR1_CEN;
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -576,14 +566,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
