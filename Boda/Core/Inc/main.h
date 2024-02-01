f5f204efd27e7ea3920dd18b7a7cc67803a0fed6
Kyle-912 <kdt912@outlook.com>
2024-01-31 20:40:28 -0500

    Corrected GPIO chip selects for SPI2, Made PB_13 GPIO for attachment, removed erroneous pin assignments (hopefully that doesn't cause problems)

--- Boda/Core/Inc/main.h
+++ Boda/Core/Inc/main.h
@@ -61,8 +61,6 @@ void Error_Handler(void);
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
-#define LD2_Pin GPIO_PIN_5
-#define LD2_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
